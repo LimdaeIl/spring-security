6ce9faf4172b365c3f0572bc33253fe6dc31f238
LimDae94 <piay801@naver.com>
2024-11-18 19:43:03 +0900

    프로젝트 초기화

---
+++ poko/src/main/java/security/book/poko/PokoApplication.java
@@ -0,0 +1,13 @@
+package security.book.poko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokoApplication.class, args);
+	}
+
+}
---
+++ poko/src/test/java/security/book/poko/PokoApplicationTests.java
@@ -0,0 +1,13 @@
+package security.book.poko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
